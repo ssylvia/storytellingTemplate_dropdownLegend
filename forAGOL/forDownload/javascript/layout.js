90aa48de4131f7d12a07bbd7cf6a54039ea8c9e7
Stephen Sylvia <ssylvia@esri.com>
2012-10-09 13:43:24 -0400

    updated api to 3.2 and updated buildLayers function

--- forAGOL/forDownload/javascript/layout.js
+++ forAGOL/forDownload/javascript/layout.js
@@ -8,19 +8,19 @@
   dojo.require("dijit.layout.ContentPane");
   dojo.require("dijit.layout.StackContainer");
   dojo.requireLocalization("esriTemplate","template");
-  
-  
+
+
      var map, urlObjects, i18n;
-	 
+
 	 function initMap() {
        patchID();
-	   
+
 	   //get the localization strings
   	   i18n = dojo.i18n.getLocalization("esriTemplate","template");
-	  
+
 	   dojo.byId('loading').innerHTML = i18n.viewer.loading.message;
 	   dojo.byId('legTogText').innerHTML = i18n.viewer.legToggle.down;
-      
+
        if(configOptions.geometryserviceurl && location.protocol === "https:"){
          configOptions.geometryserviceurl = configOptions.geometryserviceurl.replace('http:','https:');
        }
@@ -30,18 +30,18 @@
          configOptions.sharingurl = location.protocol + '//' + location.host + "/sharing/content/items";
        }
        esri.arcgis.utils.arcgisUrl = configOptions.sharingurl;
-       
-       if(!configOptions.proxyurl){   
+
+       if(!configOptions.proxyurl){
          configOptions.proxyurl = location.protocol + '//' + location.host + "/sharing/proxy";
        }
 
        esri.config.defaults.io.proxyUrl =  configOptions.proxyurl;
 
        esri.config.defaults.io.alwaysUseProxy = false;
-      
+
        urlObject = esri.urlToObject(document.location.href);
        urlObject.query = urlObject.query || {};
-      
+
        if(urlObject.query.title){
          configOptions.title = urlObject.query.title;
        }
@@ -49,14 +49,14 @@
          configOptions.title = urlObject.query.subtitle;
        }
        if(urlObject.query.webmap){
-         configOptions.webmap = urlObject.query.webmap;      
-       } 
+         configOptions.webmap = urlObject.query.webmap;
+       }
        if(urlObject.query.bingMapsKey){
-         configOptions.bingmapskey = urlObject.query.bingMapsKey;      
+         configOptions.bingmapskey = urlObject.query.bingMapsKey;
        }
 
-	   esri.arcgis.utils.arcgisUrl = "http://arcgis.com/sharing/content/items";	
-		
+	   esri.arcgis.utils.arcgisUrl = "http://arcgis.com/sharing/content/items";
+
 	   var mapDeferred = esri.arcgis.utils.createMap(configOptions.webmap, "map", {
          mapOptions: {
            slider: true,
@@ -68,15 +68,15 @@
        });
 
        mapDeferred.addCallback(function (response) {
-        
+
 		 document.title = configOptions.title|| response.itemInfo.item.title || "";
          dojo.byId("title").innerHTML = configOptions.title ||response.itemInfo.item.title || "";
          dojo.byId("subtitle").innerHTML = configOptions.subtitle|| response.itemInfo.item.snippet || "";
-        
+
          map = response.map;
-		
+
 		 dojo.connect(map,"onUpdateEnd",hideLoader);
-		
+
          var layers = response.itemInfo.itemData.operationalLayers;
          if(map.loaded){
            initUI(layers);
@@ -93,21 +93,21 @@
        mapDeferred.addErrback(function (error) {
          alert(i18n.viewer.errors.createMap + dojo.toJson(error.message));
        });
-	   
+
      }
 
 
      function initUI(layers) {
        //add chrome theme for popup
        dojo.addClass(map.infoWindow.domNode, "chrome");
-       //add the scalebar 
+       //add the scalebar
        var scalebar = new esri.dijit.Scalebar({
          map: map,
          scalebarUnit:i18n.viewer.main.scaleBarUnits //metric or english
-       }); 
+       });
 
        var layerInfo = buildLayersList(layers);
-      
+
        if(layerInfo.length > 0){
          var legendDijit = new esri.dijit.Legend({
            map:map,
@@ -119,58 +119,64 @@
          dojo.byId('legendDiv').innerHTML = "";
        }
      }
-	 
-function buildLayersList(layers){
-        //layers  arg is  response.itemInfo.itemData.operationalLayers;
-        var layerInfos = [];
-        dojo.forEach(layers, function(mapLayer, index){
-          var layerInfo = {};
-          if (mapLayer.featureCollection && mapLayer.type !== "CSV") {
-            if (mapLayer.featureCollection.showLegend === true) {
-              dojo.forEach(mapLayer.featureCollection.layers, function(fcMapLayer){
-                if (fcMapLayer.showLegend !== false) {
+
+//build a list of layers to dispaly in the legend
+  function buildLayersList(layers){
+
+ //layers  arg is  response.itemInfo.itemData.operationalLayers;
+  var layerInfos = [];
+  dojo.forEach(layers, function (mapLayer, index) {
+      var layerInfo = {};
+      if (mapLayer.featureCollection && mapLayer.type !== "CSV") {
+        if (mapLayer.featureCollection.showLegend === true) {
+            dojo.forEach(mapLayer.featureCollection.layers, function (fcMapLayer) {
+              if (fcMapLayer.showLegend !== false) {
                   layerInfo = {
-                    "layer": fcMapLayer.layerObject,
-                    "title": mapLayer.title,
-                    "defaultSymbol": false
+                      "layer": fcMapLayer.layerObject,
+                      "title": mapLayer.title,
+                      "defaultSymbol": false
                   };
                   if (mapLayer.featureCollection.layers.length > 1) {
-                    layerInfo.title += " - " + fcMapLayer.layerDefinition.name;
+                      layerInfo.title += " - " + fcMapLayer.layerDefinition.name;
                   }
                   layerInfos.push(layerInfo);
-                }
-              });
-            }
-          } else if (mapLayer.showLegend !== false) {
-            layerInfo = {
-              "layer": mapLayer.layerObject,
-              "title": mapLayer.title,
-              "defaultSymbol": false
-            };
-            //does it have layers too? If so check to see if showLegend is false
-            if (mapLayer.layers) {
-              var hideLayers = dojo.map(dojo.filter(mapLayer.layers, function(lyr){
-                return (lyr.showLegend === false);
-              }), function(lyr){
-                return lyr.id
-              });
-              if (hideLayers.length) {
-                layerInfo.hideLayers = hideLayers;
               }
-            }
-            layerInfos.push(layerInfo);
+            });
           }
-        });
-        return layerInfos;
+      } else if (mapLayer.showLegend !== false && mapLayer.layerObject) {
+      var showDefaultSymbol = false;
+      if (mapLayer.layerObject.version < 10.1 && (mapLayer.layerObject instanceof esri.layers.ArcGISDynamicMapServiceLayer || mapLayer.layerObject instanceof esri.layers.ArcGISTiledMapServiceLayer)) {
+        showDefaultSymbol = true;
       }
+      layerInfo = {
+        "layer": mapLayer.layerObject,
+        "title": mapLayer.title,
+        "defaultSymbol": showDefaultSymbol
+      };
+        //does it have layers too? If so check to see if showLegend is false
+        if (mapLayer.layers) {
+            var hideLayers = dojo.map(dojo.filter(mapLayer.layers, function (lyr) {
+                return (lyr.showLegend === false);
+            }), function (lyr) {
+                return lyr.id;
+            });
+            if (hideLayers.length) {
+                layerInfo.hideLayers = hideLayers;
+            }
+        }
+        layerInfos.push(layerInfo);
+    }
+  });
+  return layerInfos;
+  }
 
-    
+
      function patchID() {  //patch id manager for use in apps.arcgis.com
        esri.id._isIdProvider = function(server, resource) {
        // server and resource are assumed one of portal domains
- 
+
        var i = -1, j = -1;
- 
+
        dojo.forEach(this._gwDomains, function(domain, idx) {
          if (i === -1 && domain.regex.test(server)) {
            i = idx;
@@ -179,9 +185,9 @@ function buildLayersList(layers){
            j = idx;
          }
        });
- 
+
        var retVal = false;
-   
+
        if (i > -1 && j > -1) {
          if (i === 0 || i === 4) {
            if (j === 0 || j === 4) {
@@ -204,15 +210,15 @@ function buildLayersList(layers){
            }
          }
        }
- 
+
        return retVal;
-     };    
+     };
     }
-	
+
 	function hideLoader(){
 	  $("#loadingCon").hide();
 	}
-	
+
 	//Jquery Layout
 	$(document).ready(function(e) {
 	  $("#legendToggle").click(function(){
