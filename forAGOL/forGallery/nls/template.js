b85723b827d1eadbe2c32a1b0954ea91a7f122b4
ssylvia <ssylvia@esri.com>
2012-10-09 11:59:35 -0400

    first upload

---
+++ forAGOL/forGallery/nls/template.js
@@ -0,0 +1,39 @@
+﻿define({root:
+({
+  viewer:{
+    main:{
+      scaleBarUnits: "english" //"english (for miles) or "metric" (for km) - don't translate.
+    },
+    errors:{
+      createMap: "Unable to create map",
+      general: "Error",
+      bingError: "Deploying this application requires your own Bing Maps key."
+    },
+    footer:{
+    	label: "A map from:"
+    },
+    rightPane:{
+        title: "Legend",
+        message: "No operational layers"
+    },
+    leftPane:{
+        title: "Description"
+    }
+  }
+}),
+"ar":1,
+"de":1,
+"es":1,
+"fr":1,
+"it":1,
+"ja":1,
+"ko":1,
+"nl":1,
+"nb":1,
+"pl":1,
+"pt-br":1,
+"ro":1,
+"ru":1,
+"sv":1,
+"zh-cn":1
+});
